68c8641b69a56a26bfaf1378a59e66fb49da87d6
Md Rashedul Islam <santorasu09@gmail.com>
2025-06-15 23:47:55 +0600

    add theme color

--- lib/features/auth/ui/screens/login_screen.dart
+++ lib/features/auth/ui/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crafty_bay/app/app_colors.dart';
 import 'package:crafty_bay/features/auth/ui/widgets/app_logo.dart';
 import 'package:flutter/material.dart';
 
@@ -13,24 +14,28 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+
     return SafeArea(
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(children: [
-            SizedBox(height: 44,),
-            AppLogo(width: 90, height: 90),
-            Text('Welcome Back',style: TextStyle(
-              fontSize: 28,
-              fontWeight: FontWeight.w600,
-              letterSpacing: .4,
-            ),),
-            Text('Please enter your email & password',style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
-            ),),
+          child: Column(
+            children: [
+              SizedBox(height: 44),
+              AppLogo(width: 90, height: 90),
+              Text('Welcome Back', style: textTheme.titleLarge),
+              Text(
+                'Please enter your email & password',
+                style: textTheme.headlineMedium,
+              ),
+              SizedBox(height: 24),
+              TextFormField(decoration: InputDecoration()),
 
-          ]),
+              SizedBox(height: 8),
+              TextFormField(decoration: InputDecoration()),
+            ],
+          ),
         ),
       ),
     );
