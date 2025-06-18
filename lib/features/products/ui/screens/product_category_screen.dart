614c5cd28803a3d52abba173d604db5665f49305
Md Rashedul Islam <santorasu09@gmail.com>
2025-06-19 01:16:41 +0600

    Added Product Category Screen

---
+++ lib/features/products/ui/screens/product_category_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProductCategoryScreen extends StatefulWidget {
+  const ProductCategoryScreen({super.key});
+
+  static final String name = '/product-category';
+
+  @override
+  State<ProductCategoryScreen> createState() => _ProductCategoryScreenState();
+}
+
+class _ProductCategoryScreenState extends State<ProductCategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
